81e9c5a8f3e3de8d7f422fdbc11ab3a07b412c36
fgcintra <fausto@fgcintra.pro.br>
2018-10-08 20:37:04 -0300

    (08/10) Exibição de dados com pipes em Angular

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
 import { AppComponent } from './app.component';
 import { ArtigoListComponent } from './artigo/artigo-list/artigo-list.component';
 import { AppRoutingModule } from './app-routing.module';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
